331a0f2d5f447fdd80334c480b5f377584bd7856
timmcintire <tm1618>
2016-05-26 13:34:05 -0500

    add bootstrap and stylings

--- travel/map/urls.py
+++ travel/map/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from views import all_locations, editlocation
+from views import all_locations, editlocation, deletelocation
 
 urlpatterns = [
     url(r'^$', all_locations, name="locations"),
-    url(r'^location/edit/$', editlocation, name="editlocation"),
+    url(r'^location/(?P<location_id>[0-9]+)/$', editlocation, name='editlocation'),
+    url(r'^location/delete/(?P<location_id>[0-9]+)/$', deletelocation, name='deletelocation'),
 
     ]
--- travel/map/views.py
+++ travel/map/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, render_to_response, RequestContext
+from django.contrib import messages
 from models import Location
 from forms import LocationForm
 
@@ -26,19 +27,30 @@ def all_locations(request):
     return render(request, "locations.html", locals())
 
 
-def editlocation(request):
+def editlocation(request, location_id):
+    location = get_object_or_404(Location, pk=location_id)
 
     if request.POST:
-        form = LocationForm(request.POST)
+        form = LocationForm(request.POST, instance=location)
         if form.is_valid():
             form.save()
+
             return HttpResponseRedirect('/')
+
     else:
-        form = LocationForm()
+        form = LocationForm(instance=location)
 
-    location = Location.objects.all()
-    title = "Location"
-    url = "location"
+    return render_to_response("editlocation.html", {
+        'form': form,
+        'location': location,
 
-    return render(request, 'editlocation.html', locals())
+    }, context_instance=RequestContext(request))
+
+
+def deletelocation(request, location_id):
+    get_object_or_404(Location, pk=location_id).delete()
+
+    messages.success(request, 'Location Removed!')
+    return HttpResponseRedirect('/')
+
 
