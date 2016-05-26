09cc095083b18fd837db0794838e9e4dc5c78e5a
Ubuntu <ubuntu@ip-172-31-13-20.ec2.internal>
2016-05-26 18:40:37 +0000

    updates

--- travel/static/assets/js/scripts.js
+++ travel/static/assets/js/scripts.js
@@ -1,14 +1,21 @@
 
-
+/* Declar initMap function */
     function initMap() {
+        /* Declare in inital latlng variable to center map on */
         var latlng = new google.maps.LatLng(42.2847009,-33.720468 );
         var myOptions = {
             zoom: 3,
             center: latlng,
-
         };
+
+    /* Define the google map */
     var map = new google.maps.Map(document.getElementById("map"),myOptions);
+
+    /* Define information windows to show name of markers */
     var infowindow = new google.maps.InfoWindow(), marker, i;
+
+    /* Loop through django variable "marker" and "type".  Type checks for "Visited" or "Want to Visit" and defines the icon
+    type to be used based on the variable outcome */
     for (i = 0; i < markers.length; i++) {
             console.log(type[i])
             if (type[i] == "Visited") {
@@ -16,11 +23,15 @@
             } else if (type[i] == "Want to Visit") {
                 var iconVar = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
             }
+            
+        /* Defines markers on each part of the map using the markers variable defined in the django template */
         marker = new google.maps.Marker({
             position: new google.maps.LatLng(markers[i][1], markers[i][2]),
             map: map,
             icon: iconVar
         });
+
+        /*Creates the information window on mouse click */
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
                 infowindow.setContent(markers[i][0]);
